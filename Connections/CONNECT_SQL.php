45213c916299ee434dcd4694797758e73606ee4a
Pedro Henrique <pedro.veloso51158@hotmail.com>
2022-02-18 10:13:40 -0300

    Criada conexão com banco de dados E lógica para pegar todos volumes do mangá

---
+++ Connections/CONNECT_SQL.php
@@ -0,0 +1,6 @@
+<?php 
+
+// Create connection
+$conn = mysqli_connect('127.0.0.1', 'root', '', 'missingMangas');
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,46 @@
-<?php
-    echo 'Testando o deploy HEROKU';
+        <?php
+        require_once('Connections/CONNECT_SQL.php');
+        ?>
 
-?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+
+            <?php
+
+            //Query para selcionar todos os mangás e seus respectivos ultimos volumes
+            $query = "SELECT manga, ultimo_volume FROM volumes_atual;";
+            $result = mysqli_query($conn, $query);
+            $possuidos = [1, 2, 3];
+
+            //while para colocar os mangas e seus ultimos volumes em um array
+            while ($row = mysqli_fetch_array($result)) {
+                $mangasVolumes[$row['manga']] = $row['ultimo_volume'];
+            }
+
+            //foreach para separar o os mangas dos volumes
+            foreach ($mangasVolumes as $manga => $volume) {
+
+                //lógica para pegar todo o range entre o volume 1 e o ultimo volume do mangá disponível
+                $todosVolumes[$manga] = (range(1, $volume));
+
+                //lógica que pega o array de mangás possuídos e todo o range de volumes lançados do mangá para pegar o que sobra, ou seja, pegar os volumes que eu não possuo
+                $volumesFaltando = array_diff($todosVolumes[$manga], $possuidos);
+
+                print_r($manga . ':  <br>' . implode(', <br>', $volumesFaltando) . ' <br>');
+            }
+
+            ?>
+
+
+        </body>
+
+        </html>
