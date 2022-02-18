999259e662389d188ba63b33cfe5873bb748eb2d
Pedro Henrique <pedro.veloso51158@hotmail.com>
2022-02-18 10:16:23 -0300

    Adicionada chamada da API JIKAN

--- index.php
+++ index.php
@@ -37,7 +37,36 @@
 
                 print_r($manga . ':  <br>' . implode(', <br>', $volumesFaltando) . ' <br>');
             }
+            //Chamada da API JIKAN para pegar o ID do mangá
+            $curl = curl_init();
 
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://jikan1.p.rapidapi.com/search/manga?q=Attack%20on%20Titan",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => [
+                    "x-rapidapi-host: jikan1.p.rapidapi.com",
+                    "x-rapidapi-key: a73a6981bbmsh17c403bb1b0a01ap1b1f06jsn351beb61e790"
+                ],
+            ]);
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                $test = json_decode($response, true);
+                echo $test['results'][0]['mal_id'];
+            }
+            //finalização da chamada da API para pegar o ID
             ?>
 
 
